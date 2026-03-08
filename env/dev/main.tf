f986283edbb1528ca56e3ba7b7f01a6768cf4a6b
yalenthren <yalenthren2003@gmail.com>
2026-03-08 14:42:22 +0530

    Switch EC2 to Ubuntu 22.04

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -12,4 +12,3 @@ module "ec2" {
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.vpc.subnet_id
 }
-
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,6 +6,7 @@ resource "aws_security_group" "dev_sg" {
   vpc_id = var.vpc_id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -20,19 +21,29 @@ resource "aws_security_group" "dev_sg" {
   }
 }
 
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
-  owners = ["amazon"]
+  owners = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 resource "aws_instance" "dev_server" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   subnet_id = var.subnet_id
@@ -42,6 +53,6 @@ resource "aws_instance" "dev_server" {
   ]
 
   tags = {
-    Name = "terraform-ci-instance"
+    Name = "terraform-ubuntu-instance"
   }
 }
