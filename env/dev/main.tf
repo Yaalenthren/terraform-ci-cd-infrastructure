3a1cd326bf2389a92ec50abdb79f7353fe7f8b78
yalenthren <yalenthren2003@gmail.com>
2026-03-08 19:59:16 +0530

    Add Terraform remote state with S3 backend

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "yaazhan-terraform-state-bucket"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
