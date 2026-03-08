8f48359a19c59e78a6c7f839a6d8f5ab8dbeab84
yalenthren <yalenthren2003@gmail.com>
2026-03-08 20:08:01 +0530

    Fix AZ selection dynamically

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -9,6 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
