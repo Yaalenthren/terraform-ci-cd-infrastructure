5313e821e7f52e612bf544b665a0f5d01143dbdc
yalenthren <yalenthren2003@gmail.com>
2026-03-09 18:46:20 +0530

    fix CUR bucket permissions

--- modules/cost_reports/main.tf
+++ modules/cost_reports/main.tf
@@ -1,30 +1,30 @@
-resource "aws_s3_bucket" "cur_bucket" {
-  bucket = "dev-terraform-usage-report"  # must be globally unique
-  acl    = "private"
+resource "aws_s3_bucket_policy" "cur_bucket_policy" {
+  bucket = aws_s3_bucket.cur_bucket.id
 
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AllowBillingToWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "billingreports.amazonaws.com"
+        }
+        Action = [
+          "s3:GetBucketAcl",
+          "s3:GetBucketPolicy"
+        ]
+        Resource = aws_s3_bucket.cur_bucket.arn
+      },
+      {
+        Sid = "AllowBillingToPutObjects"
+        Effect = "Allow"
+        Principal = {
+          Service = "billingreports.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cur_bucket.arn}/*"
       }
-    }
-  }
-}
-
-resource "aws_cur_report_definition" "monthly_cost_report" {
-  report_name                = "dev-monthly-cost-report"
-  time_unit                  = "DAILY"
-  format                     = "textORcsv"
-  compression                = "GZIP"
-  s3_bucket                  = aws_s3_bucket.cur_bucket.bucket
-  s3_region                  = "us-east-1"
-  s3_prefix                  = "cost-reports"
-
-  additional_schema_elements = ["RESOURCES"]
-  refresh_closed_reports     = true
-  report_versioning          = "CREATE_NEW_REPORT"
+    ]
+  })
 }
